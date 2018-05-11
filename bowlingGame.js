33ab8fde499c20f252ab320d803e75ebc8e12950
bgriffin <bgriffin2365@gmail.com>
2018-05-10 23:03:01 -0400

    Final tests working

--- bowlingGame.js
+++ bowlingGame.js
@@ -8,17 +8,24 @@ BowlingGame.prototype.roll = function(pins) {
     this.rolls[this.currentRoll++] = pins;
 };
 
+
 BowlingGame.prototype.scoreGame = function() {
-    var i = 0
+    var frameIndex = 0;
+    var self = this;    
+
     for (var frame = 0; frame < 10; frame++) {
-        if (this.rolls[i] + this.rolls[i + 1] == 10) {
-            this.score += 10 + this.rolls[i + 2];
+        if (self.rolls[frameIndex] == 10) {
+            self.score += 10 + self.rolls[frameIndex + 1] + self.rolls[frameIndex + 2];
+            frameIndex += 1;
+        }
+        else if (self.rolls[frameIndex] + self.rolls[frameIndex + 1] == 10) {
+            self.score += 10 + self.rolls[frameIndex + 2];
+            frameIndex += 2;
         }
         else {   
-            this.score += this.rolls[i] + this.rolls[i + 1];
-        }
-         
-        i += 2;
+            self.score += self.rolls[frameIndex] + self.rolls[frameIndex + 1];
+            frameIndex += 2;
+        }         
     }
     
     return this.score;
--- bowlingGame.spec.js
+++ bowlingGame.spec.js
@@ -7,16 +7,20 @@ describe("BowlingGame", function() {
 
 	function rollMany (rolls, pins) {
 		for (var i = 0; i < rolls; i++) {
-			game.roll(pins)
+			game.roll(pins);
 		}
     }
     
     function rollSpare() {
         game.roll(5);
         game.roll(5);
+	}
+	
+	function rollStrike() {
+        game.roll(10);
     }
 
-	it("Can score a gutter game", () => { 
+	it("Can score a gutter game", function() { 
         rollMany(20, 0);
         expect(game.scoreGame()).to.equal(0);
     });
@@ -32,5 +36,18 @@ describe("BowlingGame", function() {
 		game.roll(3);
 		rollMany(17, 0);
 		expect(game.scoreGame()).to.equal(16);
+	});
+
+	it("should handle one strike", function() {
+		rollStrike();
+		game.roll(3);
+		game.roll(4);
+		rollMany(16, 0);
+		expect(game.scoreGame()).to.equal(24);
+	});
+
+	it("should handle a perfect game", function() {
+		rollMany(12, 10);
+		expect(game.scoreGame()).to.equal(300);
 	});
 });
