bf57317c1425c920baef37fade456ea40de3535f
bgriffin <bgriffin2365@gmail.com>
2018-05-10 21:15:15 -0400

    Test 3

--- bowlingGame.js
+++ bowlingGame.js
@@ -1,18 +1,25 @@
 var BowlingGame = function() {
     this.rolls = [];
     this.currentRoll = 0;
- };
+    this.score = 0;
+};
 
 BowlingGame.prototype.roll = function(pins) { 
     this.rolls[this.currentRoll++] = pins;
 };
 
 BowlingGame.prototype.scoreGame = function() {
-    var score = 0;
-
-    for (var i = 0; i < 20; i ++) {
-        score += this.rolls[i];
+    var i = 0
+    for (var frame = 0; frame < 10; frame++) {
+        if (this.rolls[i] + this.rolls[i + 1] == 10) {
+            this.score += 10 + this.rolls[i + 2];
+        }
+        else {   
+            this.score += this.rolls[i] + this.rolls[i + 1];
+        }
+         
+        i += 2;
     }
-
-    return score;
+    
+    return this.score;
 };
--- bowlingGame.spec.js
+++ bowlingGame.spec.js
@@ -9,7 +9,12 @@ describe("BowlingGame", function() {
 		for (var i = 0; i < rolls; i++) {
 			game.roll(pins)
 		}
-	}
+    }
+    
+    function rollSpare() {
+        game.roll(5);
+        game.roll(5);
+    }
 
 	it("Can score a gutter game", () => { 
         rollMany(20, 0);
@@ -20,5 +25,12 @@ describe("BowlingGame", function() {
 	it("should handle all ones", function() {
 		rollMany(20, 1);
 		expect(game.scoreGame()).to.equal(20);
+    });
+    
+    it("should handle one spare", function() {
+		rollSpare();
+		game.roll(3);
+		rollMany(17, 0);
+		expect(game.scoreGame()).to.equal(16);
 	});
 });
